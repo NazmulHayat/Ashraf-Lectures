e94c16d22a9d2b2c9c6e48b3d343f3a6f1bfff15
NazmulHayat <nazmulhayat588@gmail.com>
2023-12-16 17:42:48 +0600

    fixed auto scrolling problem

--- src/router/index.js
+++ src/router/index.js
@@ -58,7 +58,12 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior() {
+    return new Promise((resolve) => {
+      resolve({ left: 0, top: 0 })
+    })
+  }
 })
 
 export default router
